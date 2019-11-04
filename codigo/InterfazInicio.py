7aecc786b7154da3749d88e3cf3eba63aaa1a65a
jhernandezga <jhernandezga@unal.edu.co>
2019-11-03 23:07:36 -0500

    Se modularizó la ventana Inicio sesión

--- codigo/InterfazInicio.py
+++ codigo/InterfazInicio.py
@@ -22,19 +22,9 @@ multas = 0
 agregarProductos = False
 _USUARIO_ADMIN = "admin"
 _PASS_ADMIN = "programacion"
-rol = None
-
-
-"""CREACION DE LA VENTANA"""
-
-root = Tk()             #Se crea una ventana
-root.title("LABS UNAL")   #se le da titulo a la ventana
-root.resizable(0, 0)       # no se permite que se alargue la ventana , con (1,1) se permite alargue vertical y horizontal
-root.iconbitmap("../Imagenes/Icono/iconoUnal.ico")        #se busca la imagen del icono
-frame = Frame()         # se crea un frame
-frame.pack()            # se coloca el frame dentro de la ventana
-rol = IntVar()          #variable para almacenar lo que el usuario selecciona como rol( lo selecciona en la interfaz)
-frame.config(width="800", height="500", bg="gray")  #se configura el frame ancho, alto y color de fondo
+rol = 0
+usuario = ""
+contraseña = ""
 
 
 """FUNCION CODIGO QUE VALIDA DATOS PARA INICIAR SESION"""
@@ -106,90 +96,99 @@ class DatosUsuarios:  #Clase para la conexión y manipulación de datos de la ba
 
         return self.retorno
 
+def ventanaInicio(logo, logo2):
+    def inicioSesion():
 
-def inicioSesion():   # funcion que dice si se inicio o no sesion(comprueba usuario y contraseña)
+        usuario = usuarioEntry.get()
+        contraseña = passEntry.get()
+        rol = rola.get()
 
-        if rol.get() == 1:    # si el rol seleccionado es 1(para estudiantes) se ejecuta
-            usuario = usuarioEntry.get()   #se obtiene lo que se introdujo en la casilla de usuario en la interfaz
-            contraseña = passEntry.get()   #se obtiene lo que se introdujo en la casilla de contraseña en la interfaz
+        print(rol)
+        print(usuario)
+        print(contraseña)
+        if rol == 1:  # si el rol seleccionado es 1(para estudiantes) se ejecuta
+
             valida = DatosUsuarios()
-            validar = valida.validarContraseña(usuario,contraseña)
-            if validar :   # si inicio es verdadero se ejecuta(si es verdadero significa que coinciden los datos)
-                messagebox.showinfo("Info", "Se inició sesión correctamente")  #funcion que manda una alerta con un mensaje
+            validar = valida.validarContraseña(usuario, contraseña)
+            if validar:  # si inicio es verdadero se ejecuta(si es verdadero significa que coinciden los datos)
+                messagebox.showinfo("Info", "Correcto")
+
             else:
                 messagebox.showinfo("Info", "Usuario o contraseña incorrectos")
 
 
-        elif rol.get() == 2:  #si el rol es 2 (para administrador ) se ejecuta
+        elif rol == 2:  # si el rol es 2 (para administrador ) se ejecuta
 
-            #comprueba si lo que se introdujo en las casillas es igual a user y pass predeterminado para el administrador
-            if usuarioEntry.get() == _USUARIO_ADMIN and passEntry.get() == _PASS_ADMIN:
-                messagebox.showinfo("Info", "Se inició sesión correctamente como ADMINISTRADOR") #alerta con mensaje
+            # comprueba si lo que se introdujo en las casillas es igual a user y pass predeterminado para el administrador
+            if usuario == _USUARIO_ADMIN and contraseña == _PASS_ADMIN:
+                messagebox.showinfo("Info", "Se inició sesión correctamente como ADMINISTRADOR")  # alerta con mensaje
             else:
                 messagebox.showinfo("Info", "Usuario o contraseña de ADMINISTRADOR incorrectos")
 
-        else: # se muestra si no se selecciona el rol
+        else:  # se muestra si no se selecciona el rol
             messagebox.showinfo("Info", "Seleccione un Rol")
 
+    frame = Frame()  # se crea un frame
+    frame2 = Frame()
+    frame.pack()  # se coloca el frame dentro de la ventana
+    rola = IntVar()  # variable para almacenar lo que el usuario selecciona como rol( lo selecciona en la interfaz)
+    frame.config(width="800", height="500", bg="gray")  # se configura el frame ancho, alto y color de fondo
+    frame2.config(width="800", height="500", bg="gray")
+
+    usuarioLabel = Label(frame, text="Usuario:")    # se crea una label(para colocar texto o imagenes) y se coloca en el frame
+    usuarioLabel.config(font=("Berlin Sans FB", 18), bg="gray", fg="white")  #se configura fuente, bg color fondo y fg color letra
+    usuarioLabel.grid(row=3, column=1, padx=20, pady=20, sticky="e")  #se ubica en una grilla creada en la interfaz en fila 3 columna 1                                                                      #padx y y son para espaciado entre el cudrado de grilla
+
+    usuarioEntry = Entry(frame)     #crea un entry(caja para colocar texto) y se coloca en el frame
+    usuarioEntry.config(justify="center", fg="#7CD325", font=("Berlin Sans FB", 15)) #Se configura  justify es para justiificar el texto
+    usuarioEntry.grid(row=3, column=2, padx=10, pady=20, sticky="w")  #se ubica en grilla
+
+    passLabel = Label(frame, text="Contraseña:")
+    passLabel.config(bg="gray", fg="white", font=("Berlin Sans FB", 18))
+    passLabel.grid(row=4, column=1, padx=10, pady=20, sticky="e")
+
+    passEntry = Entry(frame)
+    passEntry.config(justify="center", show="*", fg="#7CD325", font=("Berlin Sans FB", 15))
+    passEntry.grid(row=4, column=2, padx=10, pady=20, sticky="w")
+
+     #busca una imagen en la ruta dada y la almacena en logoUnal
+    labelImagen1 = Label(frame, image=logo)  #Se crea un label y se le dice que va a contener la imagen logoUnal
+    labelImagen1.config(bg="White")
+    labelImagen1.grid(row=1, column=1)
+
+    labelImagen2 = Label(frame, image=logo2)
+    labelImagen2.config(bg="White")
+    labelImagen2.grid(row=1, column=2)
+
+    rolLabel = Label(frame, text="Seleccione su rol: ")
+    rolLabel.config(bg="gray", fg="white", font=("Berlin Sans FB", 18))
+    rolLabel.grid(row=5, column=1, padx=20, pady=20, sticky="e")
+
+    radioEstudiante = Radiobutton(frame, text="Estudiante", variable = rola, value = 1) #se crea radiobutton(el circulo para seleccionar rol)
+    radioEstudiante.config(bg="gray", font=("Berlin Sans FB", 15), activebackground="gray",
+                                       activeforeground="#7CD325", fg ="#7CD325")
+    radioEstudiante.grid(row=5, column=2, sticky="w")
+
+    radioAdministrador = Radiobutton(frame, text="Administrador", variable = rola, value = 2)
+    radioAdministrador.config(bg="gray",activebackground="gray", activeforeground="#7CD325",
+                                          font=("Berlin Sans FB", 15), fg = "#7CD325")
+    radioAdministrador.grid(row=6, column=2, sticky="w")
+
+    inicioButton = Button(frame, text="Iniciar Sesión", width=20, height=1, activeforeground="#96D646",  #se crea un boton, se le configura el texto, tamaño y color
+                                      activebackground="white", command=inicioSesion)    # command es para que llame a la funcion cuando se presione el boton
+    inicioButton.config(bg="#96D646", borderwidth=0, relief="flat", font=("Berlin Sans FB", 15), fg="white") #se configura el relieve colore y fuente
+    inicioButton.grid(row=7, column=1, columnspan=2, pady=20)# se coloca en la grilla o tabla
 
 
-"""CREACION DE WIDGETS DE LA INTERFAZ"""
 
-usuarioLabel = Label(frame, text="Usuario:")    # se crea una label(para colocar texto o imagenes) y se coloca en el frame
-usuarioLabel.config(font=("Berlin Sans FB", 18), bg="gray", fg="white")  #se configura fuente, bg color fondo y fg color letra
-usuarioLabel.grid(row=3, column=1, padx=20, pady=20, sticky="e")  #se ubica en una grilla creada en la interfaz en fila 3 columna 1
-                                                                  #padx y y son para espaciado entre el cudrado de grilla
-
-usuarioEntry = Entry(frame)     #crea un entry(caja para colocar texto) y se coloca en el frame
-usuarioEntry.config(justify="center", fg="#7CD325", font=("Berlin Sans FB", 15)) #Se configura  justify es para justiificar el texto
-usuarioEntry.grid(row=3, column=2, padx=10, pady=20, sticky="w")  #se ubica en grilla
-
-passLabel = Label(frame, text="Contraseña:")
-passLabel.config(bg="gray", fg="white", font=("Berlin Sans FB", 18))
-passLabel.grid(row=4, column=1, padx=10, pady=20, sticky="e")
-
-passEntry = Entry(frame)
-passEntry.config(justify="center", show="*", fg="#7CD325", font=("Berlin Sans FB", 15))
-passEntry.grid(row=4, column=2, padx=10, pady=20, sticky="w")
-
-logoUnal = PhotoImage(file="../Imagenes/Logos/logo.png") #busca una imagen en la ruta dada y la almacena en logoUnal
-labelImagen1 = Label(frame, image=logoUnal)  #Se crea un label y se le dice que va a contener la imagen logoUnal
-labelImagen1.config(bg="White")
-labelImagen1.grid(row=1, column=1)
-
+root = Tk()  # Se crea una ventana
+root.title("LABS UNAL")  # se le da titulo a la ventana
+root.resizable(0, 0)  # no se permite que se alargue la ventana , con (1,1) se permite alargue vertical y horizontal
+root.iconbitmap("../Imagenes/Icono/iconoUnal.ico")  # se busca la imagen del icono
+logoUnal = PhotoImage(file="../Imagenes/Logos/logo.png")
 demoUnal = PhotoImage(file="../Imagenes/Logos/demo.png")
-labelImagen2 = Label(frame, image=demoUnal)
-labelImagen2.config(bg="White")
-labelImagen2.grid(row=1, column=2)
-
-rolLabel = Label(frame, text="Seleccione su rol: ")
-rolLabel.config(bg="gray", fg="white", font=("Berlin Sans FB", 18))
-rolLabel.grid(row=5, column=1, padx=20, pady=20, sticky="e")
-
-radioEstudiante = Radiobutton(frame, text="Estudiante", variable = rol, value = 1) #se crea radiobutton(el circulo para seleccionar rol)
-radioEstudiante.config(bg="gray", font=("Berlin Sans FB", 15), activebackground="gray",
-                               activeforeground="#7CD325", fg ="#7CD325")
-radioEstudiante.grid(row=5, column=2, sticky="w")
-
-radioAdministrador = Radiobutton(frame, text="Administrador", variable = rol, value = 2)
-radioAdministrador.config(bg="gray",activebackground="gray", activeforeground="#7CD325",
-                                  font=("Berlin Sans FB", 15), fg = "#7CD325")
-radioAdministrador.grid(row=6, column=2, sticky="w")
-
-inicioButton = Button(frame, text="Iniciar Sesión", width=20, height=1, activeforeground="#96D646",  #se crea un boton, se le configura el texto, tamaño y color
-                              activebackground="white", command=inicioSesion)    # command es para que llame a la funcion cuando se presione el boton
-inicioButton.config(bg="#96D646", borderwidth=0, relief="flat", font=("Berlin Sans FB", 15), fg="white") #se configura el relieve colore y fuente
-inicioButton.grid(row=7, column=1, columnspan=2, pady=20)# se coloca en la grilla o tabla
-
+ventanaInicio(logoUnal, demoUnal)
 root.mainloop()  #debe colocarse para que la interfaz se mantenga en ejecucion y no se cierre
-
-
-"""usuario1 =Usuarios("jhernandezga","unal","Jorge Hernandez")
-data = DatosUsuarios()
-data.conectar()
-data.agregarUsuario(usuario1)"""
-#data.quitarUsuario(usuario1)
-
 
 
 
